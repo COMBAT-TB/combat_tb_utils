54dbaeb8d673beaad47bbbef6f54d9285baff1aa
Peter van Heusden <pvh@sanbi.ac.za>
2017-09-26 15:59:37 +0200

    Fix syntax error in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='combat_tb_utils',
     version='0.0.1',
-    description='Utilities modules for COMBAT TB project tools '
+    description='Utilities modules for COMBAT TB project tools',
     keywords='neo4j, neomodel',
     packages=['combat_tb_model'],
     install_requires=[
